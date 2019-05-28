520607d36edf202d31864463410dad5d1b1879d7
Cory Gilliam <imaaxa@gmail.com>
2019-05-28 18:21:08 -0400

    Add Exercise 1.1: Fix the javascript.

---
+++ week-1/gilliam-exercise-1.1.js
@@ -0,0 +1,36 @@
+/*
+==================================================
+  Title: Discussion Board Assignment: 1.1
+  Author: Cory Gilliam
+  Date: May , 2019
+  Modified By:
+  Description: This program has atleast two errors for cohorts to fix.
+  ==================================================
+*/
+
+/* Start program
+==================================================*/
+
+/*
+  Comments go here
+    * A simple JavaScript program using values and variables
+    * Has at least two errors
+    * Program should not execute
+    * Post to Blackboard
+*/
+
+/*
+  Expected output:
+    Console out, Name: Bob Smith, 123 Main St, Knoxville, TN 37921, Date: Sun Jul 28 2019 00:00:00 GMT-0400 (Eastern Daylight Time)
+*/
+
+// Variable declaration and assignment
+var name = Bob Smith;
+var address = '123 Main St, Knoxville, TN 37921';
+myDate = new Date(5, 28, 2019);
+
+// Output
+console.log("Name: " + name + ", Address: " + address + ", Date: " myDate);
+
+/*================================================
+// End program*/
