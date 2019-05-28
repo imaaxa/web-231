491f2c623ede6679716bb56bee3e7aca5ac240d1
Cory Gilliam <imaaxa@gmail.com>
2019-05-28 18:16:53 -0400

    Week 1: Discussion: Fix javascript errors.

---
+++ week-1/phillips-Assignment_1.1.js
@@ -0,0 +1,14 @@
+/*
+============================================
+; Title: Assignment 1.1
+; Author: Ernie Phillips
+; Date: 5/28/2019
+; Modified By: Cory Gilliam
+; Description: Program that produces two  errors to be fixed by a fellow student.
+;===========================================
+*/
+
+var firstName = 'Ernie'; //Declare variable of first name and set to empty string
+var lastName = 'Phillips';  //declare variable of last name and set to empty string
+
+console.log(firstName + ' ' + lastName);    //display first and last name in debugger console
