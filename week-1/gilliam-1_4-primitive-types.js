c35d3be76cd477d2c7e3f4f33fc8fcc18ecb9633
Cory Gilliam <imaaxa@gmail.com>
2019-05-28 12:56:47 -0400

    Fix file name.

---
+++ week-1/gilliam-1_4-primitive-types.js
@@ -0,0 +1,41 @@
+/*
+  Title: Primitive Types
+  Author: Cory Gilliam
+  Date: May 28, 2019
+  Modified By: Cory Gilliam
+  Description: This program logs out string and numeric variables to console.
+*/
+
+// Start program
+
+/*
+  Comments go here:
+    * Create a variable named firstName and assign it a value of "your actual first name"
+    * Create a variable named lastName and assign it a value of "your actual last name"
+    * Create a variable named age and assign it a value of "your actual age"
+    * Create a variable named street and assign it a value of "some random street address"
+    * Create a variable named city and assign it a value of "some random city name"
+    * Create a variable named zipCode and assign it a value of "some random zip code"
+    * Use the console.log function to display the contents of each variable
+    * Run and test the program and make sure your output matches what I have
+    * When you are happy with the output stage, commit, and push your work to GitHub
+*/
+
+/*
+  Expected output:
+    Output, First name: Cory, Last name: Gilliam, Age: 44, City: Knoxville, Zip: 37921, in the console
+*/
+
+// Variable declaration and assignment
+var firstName = 'Cory';
+var lastName = 'Gilliam';
+var age = 44;
+var street = '123 Main St';
+var city = 'Knoxville';
+var zipCode = 37921;
+
+// Output
+console.log('First name: ' + firstName + ', Last name: ' + lastName + ', Age: ' + age + ', City: '
++ city + ', Zip: ' + zipCode);
+
+// End program
