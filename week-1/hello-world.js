69fae26d12c24eaa5a040974aa0d5c09196b85be
Cory Gilliam <imaaxa@gmail.com>
2019-05-28 10:40:56 -0400

    Exercise 1.3 - Hello-World

---
+++ week-1/hello-world.js
@@ -0,0 +1,25 @@
+/*
+  Title: Exercise 1.3 - Hello-World
+  Author: Cory Gilliam
+  Date: May 28, 2019
+  Modified By: Cory Gilliam
+  Description: This program logs out Hello World message to console.
+*/
+
+// Start program
+
+/*
+  Comments go here
+*/
+
+/*
+  Expected output:
+    Output, Hello world, my name is Cory Gilliam., in the console
+*/
+
+// Variable declaration and assignment
+
+// Output
+console.log('Hello world, my name is Cory Gilliam.');
+
+// End program
