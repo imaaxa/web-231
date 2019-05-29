92869b858003c4f39ba8c67dd71542113857081e
Cory Gilliam <imaaxa@gmail.com>
2019-05-29 12:12:05 -0400

    Week 1: Discussion: Fix javascript errors.

---
+++ week-1/rattanavilay-week1-1.1.js
@@ -0,0 +1,26 @@
+/*
+============================================
+  Title: Week 1 - 1.1
+  Author: Thip Rattanavialy
+  Date: May 28, 2019
+  Modified By: Cory Gilliam
+  Description: There are three error in this program
+;===========================================
+*/
+/*
+Expected Output: What is Thip Rattanavilay learning in Week 1 of JavaScript I? Variables and Value
+*/
+//Start Program
+
+//Declare variable "a" to Variables string and variable "b" to Values string
+
+var a = "Variables";
+var b = "Values";
+
+//Declare variable "a" equal "a" plus "b"
+var c = (a + " and " + b);
+
+//Display Output - What is "c"
+console.log("What is Thip Rattanavilay learning in Week 1 of JavaScript I? " + c);
+
+//End of Program
