eb6f200388e95b2db4f5b2f43c844f24a365bc6e
Cory Gilliam <imaaxa@gmail.com>
2019-06-04 18:24:53 -0400

    Added Exercise 2.2 - Header files.

---
+++ week-2/gilliam-header-test.js
@@ -0,0 +1,15 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Cory Gilliam
+; Date:   4 June 2019
+; Modified By: Cory Gilliam
+; Description: Displays a formatted header
+;===========================================
+*/
+
+// Import the header file
+const header = require('../gilliam-header.js');
+
+// Output
+console.log(header.display('Cory', 'Gilliam', 'Exercise 2.2 - Header'));
