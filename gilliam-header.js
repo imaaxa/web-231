2ce15625f26e9922152c8b2090ef601612410b6f
Cory Gilliam <imaaxa@gmail.com>
2019-06-25 16:59:46 -0400

    OCD forced me to fix spacing.

--- gilliam-header.js
+++ gilliam-header.js
@@ -14,8 +14,5 @@
 * Description: Returns a well-formatted string header
 */
 exports.display = function (firstName, lastName, assignment) {
-  let output = '\n' + firstName + ' ' + lastName + '\n' + assignment + '\nDate: ' +
-  new Date().toLocaleDateString('en-US')
-
-  return output
+  return '\n' + firstName + ' ' + lastName + '\n' + assignment + '\nDate: ' + new Date().toLocaleDateString('en-US');
 }
