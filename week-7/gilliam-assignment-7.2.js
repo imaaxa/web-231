2864ee113ea6ece6c69547a87ce5d5234f3ef4bd
Cory Gilliam <imaaxa@gmail.com>
2019-07-10 08:52:55 -0400

    Minor fix

--- week-7/gilliam-assignment-7.2.js
+++ week-7/gilliam-assignment-7.2.js
@@ -23,7 +23,7 @@ console.log(header.display('Cory', 'Gilliam', 'Exercise 7.2') + '\n');
 
   Employee ID: 1, Name: Gandalf The Grey, Title: Wizard
   Employee ID: 2, Name: Frodo Baggins, Title: Ring Bearer
-  Employee ID: 3, Name: Samwise Gamged, Title: Best Friend
+  Employee ID: 3, Name: Samwise Gamgee, Title: Best Friend
   Employee ID: 4, Name: Meriadoc Brandybuck, Title: Friend
   Employee ID: 5, Name: Peregrin Took, Title: Friend
 */
@@ -42,13 +42,9 @@ function Employee(id, firstName, lastName, title) {
 // Variables:
 var employees = [
   new Employee(1,"Gandalf","The Grey","Wizard"),
-
   new Employee(2, "Frodo", "Baggins", "Ring Bearer"),
-
-  new Employee(3,"Samwise","Gamged","Best Friend"),
-
+  new Employee(3,"Samwise","Gamgee","Best Friend"),
   new Employee(4,"Meriadoc","Brandybuck","Friend"),
-
   new Employee(5,"Peregrin","Took","Friend")
 ];
 
